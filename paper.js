f94fabb55cf07c2bd712ce6f0fdebe5ebfbb46bd
Bob Ziroll <elderziroll@gmail.com>
2016-04-14 11:10:54 -0600

    Added a few more errors

--- paper.js
+++ paper.js
@@ -2,41 +2,37 @@
 
 var Enemies=["Lex","Batman","Darkseid","Braniac","General Zod","Doomsday"];
 
-function Who_Wins(is-there-kryptonite,enemy_name)
+funtion Who_Wins(is-there-kryptonite,enemy_name)
 {
 if (!is-there-kryptonite) 
 {
     return "Superman beats " +enemyName+ ", of course";
 } 
 
-else
-{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.";
+else{
+    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
 }
     }
 
 for (var i=0;i<enemies.length;i++) 
 {
-    var is_there_kryptonite=(i%2===0);
+    var is_there_kryptonite=(i%2=0);
     console.log(Who_Wins(is_there_kryptonite,enemies[i]));
 }
 
 function HowAttractedIsLoisLaneToMe () 
 {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Math.random()*10)+1);
+    return Math.floor((Mat.random()*10)+1);
 }
 
 var clarkKent =true;
-var superman= false;
+var superman false;
 
-while (clarkKent) 
-{
+while (clarkKent){
     console.log("I'm just a nerdy columnist");
     var phoneBoothQuickChange =Math.random();
     if (phoneBoothQuickChange>= 0.5) {
-        clarkKent=false;
+        clarkKent===false;
         superman=true;
-        console.log("Now I'm Superman!");
-    }
-}
+        console.log("Now I'm Superman!");}}
