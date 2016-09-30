54ca9733559c806822229a3a50ef240ff5d8c0af
Bob Ziroll <elderziroll@gmail.com>
2016-09-30 14:42:00 -0600

    Added line to emphasize a syntax error when run in node

--- paper.js
+++ paper.js
@@ -2,7 +2,7 @@
 This code contains only syntax and code style problems. The logic of the code works,
 so DO NOT change the functionality of anything in here. For example, the `while` loop
 at the end has syntax problems, not logic problems. It shouldn't be an infinite loop,
-but the way to fix that is not to change the structure of the while loop. 
+but the way to fix that is not to change the structure of the while loop.
 
 In short, you shouldn't need to add your own statements anywhere,
 just fix the existing ones.
@@ -11,31 +11,33 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var Enemies=["Lex","Batman","Darkseid","Braniac","General Zod","Doomsday"];
+var Enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
 
 funtion Who_Wins(is-there-kryptonite,enemy_name)
 {
-if (!is-there-kryptonite) 
+if (!is-there-kryptonite)
 {
     return "Superman beats " +enemyName+ ", of course";
-} 
+}
 
 else{
     return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
 }
     }
 
-for (var i=0;i<enemies.length;i++) 
+for (var i=0;i<enemies.length;i++)
 {
     var is_there_kryptonite=(i%2=0);
     console.log(Who_Wins(is_there_kryptonite,enemies[i]));
 }
 
-function HowAttractedIsLoisLaneToMe () 
+function HowAttractedIsLoisLaneToMe ()
 {
     // 1 is not at all attracted, 10 is "super" attracted...
     return Math.floor((Mat.random()*10)+1);
 }
+
+consol.log( HowAttractedIsLoisLaneToMe ()) ;
 
 var clarkKent =true;
 var superman false;
