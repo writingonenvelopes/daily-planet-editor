2e7b44565267653127e7d05bee7ad5f954ae28ed
andria <writingonenvelopes@gmail.com>
2020-09-09 16:51:09 -0400

    revision
    debugging paper.js

--- paper.js
+++ paper.js
@@ -9,41 +9,37 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var Enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var enemies= ["Lex", "Batman", "Darkside", "Brainiac", "General Zod", "Doomsday" ];
+var hero= "Superman";
+function Who_Wins (is_there_kryptonite, enemies)
+{
 
-funtion Who_Wins(is-there-kryptonite,enemy_name)
-{
-if (!is-there-kryptonite)
-{
-    return "Superman beats " +enemyName+ ", of course";
+            if (!is_there_kryptonite) { 
+                return "Superman beats " + enemies + ", of course";  
+            }else {
+                return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemies + " could possibly win this one.";
+            }
 }
-
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
-}
-    }
-
-for (var i=0;i<enemies.length;i++)
-{
-    var is_there_kryptonite:
-    if (i%2=== 0) {
+console.log(Who_Wins)
+for (var i=0 ; i < enemies.length ; i++ ) {
+    var is_there_kryptonite;
+    if ( i% 2=== 0) {
         is_there_kryptonite = true;
-    } else {
+        } else {
         is_there_kryptonite = false;
     }
-    console.log(Who_Wins(is_there_kryptonite,enemies[i]));
+    console.log(Who_Wins (is_there_kryptonite,enemies[i]) );
 }
 
-function HowAttractedIsLoisLaneToMe ()
+function HowAttractedIsLoisLaneToMe()
 {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Mat.random()*10)+1);
+    return Math.floor((Math.random()*10)+1);
 }
-
-consol.log( HowAttractedIsLoisLaneToMe ()) ;
+console.log( HowAttractedIsLoisLaneToMe ) ;
 
 var clarkKent =true;
-var superman false;
+var superman =false;
 
 while (clarkKent){
     console.log("I'm just a nerdy columnist");
@@ -51,4 +47,6 @@ while (clarkKent){
     if (phoneBoothQuickChange>= 0.5) {
         clarkKent=false;
         superman=true;
-        console.log("Now I'm Superman!");}}
+        console.log("Now I'm Superman!")
+    }
+}
